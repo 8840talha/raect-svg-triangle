f2bf78f42b2e258e8e5dd3f9b79e79fed060e6cb
TALHA <talhaparvez285@gmail.com>
2021-05-04 12:15:36 +0530

    Add input field to change sides and shape of triangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import Triangle from "./components/Triangle";
 
 function App() {
+  const [sideA, setSideA] = useState("200,10");
+  const [sideB, setSideB] = useState("250,190");
+  const [sideC, setSideC] = useState("160,210");
   return (
     <div style={{ color: "black" }}>
+      <input
+        type="text"
+        onChange={(e) => setSideA(e.target.value)}
+        placeholder="x,y for a"
+      />
+      <input
+        type="text"
+        onChange={(e) => setSideB(e.target.value)}
+        placeholder="x,y for b"
+      />
+      <input
+        type="text"
+        onChange={(e) => setSideC(e.target.value)}
+        placeholder="x,y for c"
+      />
       <svg width="500" height="500">
-        <Triangle a="200,10" b="250,190" c="160,210" />
+        <Triangle a={sideA} b={sideB} c={sideC} />
       </svg>
     </div>
   );
